b7fa09a9e8678ad4a1858a5f3989817c0b592f6b
shongCoder <soyeong163@gmail.com>
2024-10-21 16:43:24 +0900

    PSY_FAQRegisterComponent ì :t: 등록 화면 ã구현

---
+++ src/components/faq/FAQRegisterComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" />
+    <input type="text" />
+    <button>완료</button>
+  </div>
+</template>
+
+<script setup>
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+
+const faq = ref({
+  question: "",
+  answer: "",
+});
+
+const handleClickComplate = async () => {
+  await postFAQOne(faq.value);
+  router.replace("/faq/list");
+}
+
+</script>
+
+<style scoped>
+
+</style>
