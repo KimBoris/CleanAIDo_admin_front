b532803e8bd474c28e846fefc40caf51b6b6cfbc
Hyeonjun Park <baghyeonjun16@gmail.com>
2024-11-26 14:16:22 +0900

    PHJ_pinin setting and qnaApi에 테스트 용 코드 작성

--- src/main.js
+++ src/main.js
@@ -4,5 +4,9 @@ import router from "./routers/index.js";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import {createPinia} from "pinia";
 
-createApp(App).use(router).mount('#app');
+createApp(App)
+    .use(router)
+    .use(createPinia())
+    .mount('#app');
