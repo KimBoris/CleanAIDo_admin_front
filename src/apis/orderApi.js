8bc92b118b8b2125e138283e84b92e8b40b03084
Taehwan Kim <kyb062855@naver.com>
2024-11-28 14:50:43 +0900

    merge

--- src/apis/orderApi.js
+++ src/apis/orderApi.js
@@ -3,12 +3,23 @@ import {useAuthStore} from "../stores/useAuthStore.js";
 
 const host = 'http://localhost:8080/api/v1/admin/orders';
 
+// export const getInProgressOrders = (page = 1, size = 10, searchType = '', keyword = '') => {
+//     const authStore = useAuthStore();
+//     const accessToken = authStore.accessToken
+//     return axios.get(`${host}/in-progress`, {
+//         Authorization: `Bearer ${accessToken}`, // accessToken을 헤더에 추가
+//         params: { page, size, searchType, keyword }
+//     });
+// };
+
 export const getInProgressOrders = (page = 1, size = 10, searchType = '', keyword = '') => {
     const authStore = useAuthStore();
-    const accessToken = authStore.accessToken
+    const accessToken = authStore.accessToken;
     return axios.get(`${host}/in-progress`, {
-        Authorization: `Bearer ${accessToken}`, // accessToken을 헤더에 추가
-        params: { page, size, searchType, keyword }
+        params: { page, size, searchType, keyword },
+        headers: {
+            Authorization: `Bearer ${accessToken}` // 헤더에 추가
+        },
     });
 };
 
