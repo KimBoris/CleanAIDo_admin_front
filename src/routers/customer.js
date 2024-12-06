d2e120a065386dd16c7a731d7883a77587a9b96f
KimBoris <zlas2345@gmail.com>
2024-12-06 13:11:31 +0900

    KBK_customer, customerListComponent - customer ReadPage modal, contextMenu

--- src/routers/customer.js
+++ src/routers/customer.js
@@ -4,7 +4,7 @@ const CustomerListPage = () => import("../pages/customer/CustomerListPage.vue");
 const customerRouter = {
     path: "/customer",
     component: IndexPage,
-    meta: {title: "사용자(customer)"},
+    meta: {title: "고객(customer)"},
     children: [
         {path: "", redirect: '/customer/list'},
         {path: "list", component: CustomerListPage, meta: {back: "none"}}
